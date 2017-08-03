27a027366de5bea7ee7638795650ecf7517d4419
Andhika Adhitama Gama <agandhika@gmail.com>
2017-08-03 18:12:34 +0700

    update

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+
+	// Start server
+	e.Start(":7723")
+}
