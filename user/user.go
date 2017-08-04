3db100bfd07d2941a05bc4b4128794e84f1f6b4e
Andhika Adhitama Gama <agandhika@gmail.com>
2017-08-04 17:59:24 +0700

    update user struct

--- user/user.go
+++ user/user.go
@@ -1,6 +1,11 @@
 package user
 
 type User struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID        string `json:"id"`
+	Username  string `json:"username"`
+	Fullname  string `json:"fullname"`
+	DOB       string `json:"dob"`
+	Gender    string `json:"gender"`
+	Source    string `json:"source"`
+	Activated bool   `json:"activated"`
 }
